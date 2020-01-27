1e10d7a9404753ddf8ebab7fbb4e255dc2a4037f
Ossi <ossi.j.lehtonen@helsinki.fi>
2020-01-27 12:01:43 +0200

    add needed imports

--- application/__init__.py
+++ application/__init__.py
@@ -4,9 +4,8 @@ app = Flask(__name__)
 
 from flask_sqlalchemy import SQLAlchemy
 
-
+# Database
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///forum.db"
-
 app.config["SQLALCHEMY_ECHO"] = True
 
 
@@ -14,5 +13,25 @@ db = SQLAlchemy(app)
 
 from application import views
 from application.threads import models, views
+from application.auth import models
+from application.auth import views
 
+# Log in
+from application.auth.models import User
+from os import urandom
+app.config["SECRET_KEY"] = urandom(32)
+
+
+from flask_login import LoginManager
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+login_manager.login_view = "auth_login"
+login_manager.login_message = "Please login to use this functionality."
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
+
+# Create db tables if they don't exist
 db.create_all()
