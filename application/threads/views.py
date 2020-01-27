da705f6a7090c43d047abed16510ce33d95cca29
Ossi <ossi.j.lehtonen@helsinki.fi>
2020-01-27 12:00:18 +0200

    add login_required tag to certain pages

--- application/threads/views.py
+++ application/threads/views.py
@@ -1,8 +1,10 @@
 from application import app, db
 from flask import redirect, render_template, request, url_for
+from flask_login import login_required
+
 from application.threads.models import Thread
 from datetime import datetime
-
+from application.threads.forms import ThreadForm
 
 @app.route("/threads", methods=["GET"])
 def threads_index():
@@ -11,16 +13,19 @@ def threads_index():
 
 
 @app.route("/threads/edit/<thread_id>/", methods=["POST"])
+@login_required
 def threads_edit(thread_id):
 
     return redirect(url_for('threads_openedit', thread_id = thread_id))
 
 @app.route("/threads/edit/<thread_id>", methods=["GET"])
+@login_required
 def threads_openedit(thread_id):
     thread = Thread.query.get(thread_id)
     return render_template("threads/edit.html", thread = thread)
 
 @app.route("/threads/edit/<thread_id>/conf", methods=["POST"])
+@login_required
 def threads_confirmedit(thread_id):
     thread = Thread.query.get(thread_id)
     thread.title = request.form.get("title")
@@ -33,12 +38,20 @@ def threads_confirmedit(thread_id):
 
 
 @app.route("/threads/new")
+@login_required
 def threads_form():
-    return render_template("threads/new.html")
+    return render_template("threads/new.html", form = ThreadForm())
 
 @app.route("/threads", methods=["POST"])
+@login_required
 def threads_create():
-    thread = Thread(request.form.get("title"))
+    form = ThreadForm(request.form)
+
+    if not form.validate():
+        return render_template("threads/new.html", form = form)
+
+
+    thread = Thread(form.title.data)
 
     db.session().add(thread)
     db.session().commit()
