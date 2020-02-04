7becd50b5c7b23bbe2f79055e101a6113104069d
Ossi <ossi.j.lehtonen@helsinki.fi>
2020-02-04 10:46:48 +0200

    add routes to new functionality

--- application/threads/views.py
+++ application/threads/views.py
@@ -2,6 +2,10 @@ from application import app, db
 from flask import redirect, render_template, request, url_for
 from flask_login import login_required, current_user
 
+from application.comments.models import Comment
+from application.comments.models import Comment
+from application.comments.forms import CommentForm
+from application.auth.models import User
 from application.threads.models import Thread
 from datetime import datetime
 from application.threads.forms import NewThreadForm
@@ -11,7 +15,28 @@ from application.threads.forms import EditThreadForm
 def threads_index():
     return render_template("threads/list.html", threads = Thread.query.all())
 
+@app.route("/threads/read/<thread_id>", methods=["POST"])
+@login_required
+def threads_comment(thread_id):
+    form = CommentForm(request.form)
+    comment = Comment(form.comment.data)
+    comment.thread_id = thread_id
+    comment.user_id = current_user.id
+    db.session().add(comment)
+    db.session().commit()
 
+    return threads_open(thread_id)
+
+@app.route("/threads/read/<thread_id>", methods=["GET"])
+def threads_open(thread_id):
+    thread = Thread.query.get(thread_id)
+    comments = Comment.connect_users_and_comments(thread_id)
+
+    
+    user = User.query.get(thread.user_id)
+    
+    
+    return render_template("threads/showthread.html", form = CommentForm(), comments = comments, thread = thread, user = user)
 
 @app.route("/threads/edit/<thread_id>")
 @login_required
@@ -25,7 +50,7 @@ def threads_openedit(thread_id):
 def threads_confirmedit(thread_id):
     thread = Thread.query.get(thread_id)
     form = EditThreadForm(request.form)
-
+    
     if not form.validate():
         return render_template("threads/edit.html", thread = thread, form = form)
 
@@ -50,7 +75,10 @@ def threads_delete(thread_id):
 @app.route("/threads/new")
 @login_required
 def threads_form():
+    
     return render_template("threads/new.html", form = NewThreadForm())
+
+
 
 @app.route("/threads", methods=["POST"])
 @login_required
@@ -63,7 +91,16 @@ def threads_create():
 
     thread = Thread(form.title.data)
     thread.user_id = current_user.id
+    thread.description = form.description.data
     db.session().add(thread)
     db.session().commit()
 
     return redirect(url_for("threads_index"))
+
+@app.route("/threads/mythreads")
+@login_required
+def threads_openmythreads():
+    user = current_user
+    threads = Thread.query.filter_by(user_id=user.id)
+
+    return render_template("threads/mythreads.html", ownthreads = threads)
