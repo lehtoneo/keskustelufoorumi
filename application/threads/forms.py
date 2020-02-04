3fda017755e43a228df8e18289092c0d0f2b1938
Ossi <ossi.j.lehtonen@helsinki.fi>
2020-02-04 10:46:12 +0200

    add description to new thread form

--- application/threads/forms.py
+++ application/threads/forms.py
@@ -1,14 +1,16 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, validators
+from wtforms import StringField, validators, TextAreaField
 
 class NewThreadForm(FlaskForm):
     title = StringField("Thread title", [validators.Length(min=5)])
+    description = TextAreaField("Description")
  
     class Meta:
         csrf = False
 
 class EditThreadForm(FlaskForm):
     title = StringField("New title: ", [validators.Length(min=5)])
+    description = TextAreaField("Description")
     
  
     class Meta:
