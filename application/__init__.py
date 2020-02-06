7b594531f89078a190ab683a5ca1a71158e6ab2d
Ossi <ossi.j.lehtonen@helsinki.fi>
2020-02-06 10:15:31 +0200

    import statistics

--- application/__init__.py
+++ application/__init__.py
@@ -21,6 +21,7 @@ from application.threads import views
 from application.comments import models
 from application.auth import models
 from application.auth import views
+from application.statistics import views
 
 # Log in
 from application.auth.models import User
