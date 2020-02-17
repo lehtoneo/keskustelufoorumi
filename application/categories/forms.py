df426c1588a73479ccc1106d406d80d5faf74f61
Ossi <ossi.j.lehtonen@helsinki.fi>
2020-02-17 11:33:05 +0200

    make form dynamic

--- application/categories/forms.py
+++ application/categories/forms.py
@@ -2,14 +2,16 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, validators, SelectField
 from application.categories.models import Category
 
-class CategoryForm(FlaskForm):
+def CategoryForm(requestform):
+    class TempForm(FlaskForm):
     
-    categories = SelectField(u'Select Category', choices=Category.findChoices())
-    othercategory = StringField("If you selected 'other' from the preceding list, type new category here. Otherwise, leave this empty", [validators.Length(min=5, max=30)])
-    
+        othercategory = StringField("If you selected 'other' from the preceding list, type new category here. Otherwise, leave this empty", [validators.Length(min=5, max=30)])
 
-    
+        class Meta:
+            csrf = False
+    setattr(TempForm, 'categories', SelectField(u'Select Category', choices=Category.findChoices()))
+    if(requestform == None):
+        return TempForm()
+        
 
-    
-    class Meta:
-        csrf = False
+    return TempForm(requestform)
