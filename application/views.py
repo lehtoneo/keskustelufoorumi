6e1772510180fb8ef4c03ab09e8e4d18938c6620
Ossi <ossi.j.lehtonen@helsinki.fi>
2020-02-23 11:50:20 +0200

    add most recent posts to homepage

--- application/views.py
+++ application/views.py
@@ -1,6 +1,10 @@
 from flask import render_template
-from application import app
+from application import app, db
+from sqlalchemy import desc
+
+from application.threads.models import Thread, Thread_Category
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    threads = Thread.query.order_by(desc(Thread.posted)).limit(3).all()
+    return render_template("index.html", threads = threads)
